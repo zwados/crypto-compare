4bc502974c8abfc35c5673420c3428c08d67b58d
zwados <lukasz.rozwadowski7@gmail.com>
2019-09-03 17:44:47 +0200

    restructured filed, removed boilerplate

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
